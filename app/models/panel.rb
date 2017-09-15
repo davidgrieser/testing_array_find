d4d93f5839115fb5cd85abafcaef12c4224ebad2
David Grieser <david.grieser@gmail.com>
2017-09-14 20:23:13 -0500

    working with all unique values

--- app/models/panel.rb
+++ app/models/panel.rb
@@ -7,6 +7,18 @@ class Panel < ApplicationRecord
 
   def self.find_matching_antibodies(antibodies)
     return [] if antibodies.blank?
-    Panel.joins(:antibodies).where(antibodies: {name: antibodies.map(&:name)})
+    # Panel.joins(:antibodies).where(antibodies: {name: antibodies.map(&:name)})
+
+    panels = []
+    # panel_set_a = Panel.joins(:antibodies).where(antibodies: { name: set_2[1].name })
+    antibodies.each do |antibody|
+      panels << Panel.joins(:antibodies).where(antibodies: { name: antibody.name })
+    end
+
+    panels << Panel.joins(:antibodies).group('panels.id').having("count(panel_id) = #{antibodies.count}")
+    full_sql = panels.map(&:to_sql).join(" intersect ")
+
+    # Panel.find_by_sql("#{panel_set_a.to_sql} intersect #{panel_set_b.to_sql} intersect #{result_matching_count.to_sql}")
+    Panel.find_by_sql(full_sql)
   end
 end
--- spec/models/panel_spec.rb
+++ spec/models/panel_spec.rb
@@ -2,6 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Panel, type: :model do
   describe '.find_matching_antibodies' do
+    def create_antibody(letter)
+      Antibody.new(
+        name: "Antibody #{letter}",
+        amount: 9.99
+      )
+    end
+    
+    def create_antibodies(letters)
+      antibodies = []
+      letters.each do |letter|
+        antibodies << create_antibody(letter)
+      end
+      antibodies
+    end
+
+    let!(:sample_panel_single_antibody) {
+      Panel.create(
+        name: 'Sample Panel Single Antibody',
+        antibodies: [create_antibody('A')]
+      )
+    }
+    let!(:sample_panel_two_antibodies) {
+      Panel.create(
+        name: 'Sample Panel Two Antibodies',
+        antibodies: create_antibodies(%w(A B))
+      )
+    }
+    let!(:sample_panel_three_antibodies) {
+      Panel.create(
+        name: 'Sample Panel Three Antibodies',
+        antibodies: create_antibodies(%w(A B C))
+      )
+    }
+
     context 'given an empty array' do
       it 'should return []' do
         expect(Panel.find_matching_antibodies([])).to eq([])
@@ -9,103 +43,76 @@ RSpec.describe Panel, type: :model do
     end
 
     context 'given an array with 1 antibody' do
-      context 'given a panel that has only that antibody' do
-        it 'should return that panel' do
-          panel = Panel.create(name: 'Sample Panel')
-          antibody = Antibody.create(
-              name: 'Antibody',
-              amount: 9.99,
-              panel: panel
-          )
+      context 'with a panel that has only that antibody' do
+        subject(:panel) { Panel.find_matching_antibodies([create_antibody('A')]) }
 
-          expect(Panel.find_matching_antibodies([antibody])).to eq([panel])
+        it 'should return that panel' do
+          expect(panel.count).to eq(1)
+          expect(panel).to eq([sample_panel_single_antibody])
         end
       end
-      context 'given a panel that has only a different antibody' do
-        it 'should return []' do
-          panel_one = Panel.create(name: 'Sample Panel One')
-          antibody_one = Antibody.create(
-              name: 'Antibody One',
-              amount: 9.99,
-              panel: panel_one
-          )
-          antibody_two = Antibody.new(
-              name: 'Antibody Two',
-              amount: 9.99
-          )
 
-          expect(Panel.find_matching_antibodies([antibody_two])).to eq([])
+      context 'with a panel that has a different antibody' do
+        subject(:panel) { Panel.find_matching_antibodies([create_antibody('D')]) }
+
+        it 'should return []' do
+          expect(panel).to eq([])
         end
       end
     end
 
-    context 'given an array with 2 anitbodies' do
-      context 'given a matching set of antibodies on a panel' do
-        it 'should return that panel' do
-          panel_one = Panel.create(name: 'Sample Panel One')
-          antibody_one = Antibody.create(
-              name: 'Antibody One',
-              amount: 9.99,
-              panel: panel_one
-          )
-          antibody_two = Antibody.new(
-              name: 'Antibody Two',
-              amount: 9.99,
-              panel: panel_one
-          )
-          expect(Panel.find_matching_antibodies([antibody_one, antibody_two])).to eq([panel_one])
-        end
+    context 'with a panel having antibodies A and B' do
+      context 'given an array with antibodies A and B' do
+        subject(:panel) { Panel.find_matching_antibodies(create_antibodies(%w(A B))) }
 
-        context 'antibodies out of order' do
-          it 'should return that panel' do
-            panel_one = Panel.create(name: 'Sample Panel One')
-            antibody_one = Antibody.create(
-                name: 'Antibody One',
-                amount: 9.99,
-                panel: panel_one
-            )
-            antibody_two = Antibody.new(
-                name: 'Antibody Two',
-                amount: 9.99,
-                panel: panel_one
-            )
-            expect(Panel.find_matching_antibodies([antibody_two, antibody_one])).to eq([panel_one])
-          end
+        it 'should return that panel' do
+          expect(panel.count).to eq(1)
+          expect(panel).to eq([sample_panel_two_antibodies])
+        end
+      end
+
+      context 'given an array with antibodies B and A' do
+        subject(:panel) { Panel.find_matching_antibodies(create_antibodies(%w(B A))) }
+
+        it 'should return that panel' do
+          expect(panel.count).to eq(1)
+          expect(panel).to eq([sample_panel_two_antibodies])
+        end
+      end
+
+      context 'given an array with antibody D' do
+        subject(:panel) { Panel.find_matching_antibodies([create_antibody('D')]) }
+
+        it 'should return []' do
+          expect(panel).to eq([])
         end
       end
 
       context 'given a panel with 2 antibodies that do not match' do
+        subject(:panel) { Panel.find_matching_antibodies(create_antibodies(%w(D E))) }
+
         it 'should return []' do
-          panel_one = Panel.create(name: 'Sample Panel One')
-          antibody_one = Antibody.create(
-              name: 'Antibody One',
-              amount: 9.99,
-              panel: panel_one
-          )
-          antibody_two = Antibody.create(
-              name: 'Antibody Two',
-              amount: 9.99,
-              panel: panel_one
-          )
-          antibody_three = Antibody.new(
-              name: 'Antibody Three',
-              amount: 9.99
-          )
-          antibody_four = Antibody.new(
-              name: 'Antibody Four',
-              amount: 9.99
-          )
-          expect(Panel.find_matching_antibodies([antibody_three, antibody_four])).to eq([])
+          expect(panel).to eq([])
         end
       end
     end
 
-    context 'given an array with 3 anitbodies' do
-      context 'given a matching set of antibodies on a panel' do
-        it 'should return that panel'
+    context 'with a panel having anitbodies A, B, and C' do
+      context 'given an array with antibodies A, B, and C' do
+        subject(:panel) { Panel.find_matching_antibodies(create_antibodies(%w(A B C))) }
+
+        it 'should return that panel' do
+          expect(panel.count).to eq(1)
+          expect(panel).to eq([sample_panel_three_antibodies])
+        end
       end
+
       context 'given a panel with 3 antibodies that do not match' do
-        it 'should return []'
+        subject(:panel) { Panel.find_matching_antibodies(create_antibodies(%w(D E F))) }
+
+        it 'should return []' do
+          expect(panel).to eq([])
+        end
       end
     end
   end
