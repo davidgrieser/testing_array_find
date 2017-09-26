85ad4df57106fd38c867adc52c41c44308c0f9e9
David Grieser <david.grieser@gmail.com>
2017-09-25 23:16:09 -0500

    annotate with rake task installed

--- app/models/panel.rb
+++ app/models/panel.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: panels
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Panel < ApplicationRecord
   has_many :antibodies
 
